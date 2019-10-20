437e9db1e3c959f1c30077f9391a0e788342818c
unknown <kamilszymborski@mail.com>
2019-10-20 16:01:02 +0200

    1.0.8 added documentation for implemented class

--- Priority.cs
+++ Priority.cs
@@ -1,9 +1,13 @@
 ﻿namespace KamilSzymborski.MailSenders
 {
+    /// <include file=".Docs/.Priority.xml" path="docs/header"/>
     public enum Priority
     {
+        /// <include file=".Docs/.Priority.xml" path="docs/value[@name='Low']/*"/>
         Low = -1,
+        /// <include file=".Docs/.Priority.xml" path="docs/value[@name='Normal']/*"/>
         Normal = 0,
+        /// <include file=".Docs/.Priority.xml" path="docs/value[@name='High']/*"/>
         High = 1
     }
 }
